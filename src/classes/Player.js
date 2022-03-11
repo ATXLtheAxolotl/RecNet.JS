41d44512d52cd0191468b7afd2620de2af2925a7
ATXLtheAxolotl <flashtube107@gmail.com>
2022-03-10 20:30:39 -0500

    Remove uncecessary functions. What in world was I thinking when I made that?

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,49 +1,28 @@
 import axios from "axios";
 
 export class Player  {
-    username;
-    id
-    dn;
-    pi;
-    junior;
-    platform;
-    pronouns;
-    identity;
-    creation;
+    Username;
+    ID
+    DisplayName;
+    ProfileImage;
+    isJunior;
+    Platforms;
+    PersonalPronouns;
+    IdentityFlags;
+    CreationDate;
 
     constructor(ID, Username, DisplayName, ProfileImage, isJunior, Platforms, PersonalPronouns, IdentityFlags, CreationDate) {
-        this.id = ID
-        this.username = Username;
-        this.dn = DisplayName;
-        this.pi = ProfileImage;
-        this.junior = isJunior;
-        this.platform = Platforms;
-        this.pronouns = PersonalPronouns;
-        this.identity = IdentityFlags;
-        this.creation = CreationDate;
+        this.ID = ID;
+        this.Username = Username;
+        this.DisplayName = DisplayName;
+        this.ProfileImage = ProfileImage;
+        this.isJunior = isJunior;
+        this.Platforms = Platforms;
+        this.PersonalPronouns = PersonalPronouns;
+        this.IdentityFlags = IdentityFlags;
+        this.CreationDate = new Date(CreationDate);
     }
 
-    getUsername() {
-        return this.username;
-    }
-    getDisplayName() {
-        return this.dn;
-    }
-    getProfilePictureResourceName() {
-        return this.pi;
-    }
-    isJunior() {
-        return this.junior;
-    }
-    getPlatform() {
-        return this.platform
-    }
-    getIdentity() {
-        return this.identity;
-    }
-    getCreationDate() {
-        return new Date(this.creation);
-    }
     getBio() {
         return new Promise(res => {
             axios.get(`https://accounts.rec.net/account/${this.id}/bio`).then((r)=> {
