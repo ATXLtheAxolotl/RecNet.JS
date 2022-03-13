6b74a7872742f4e8e2e3efe775c4c4492002b785
ATXLtheAxolotl <flashtube107@gmail.com>
2022-03-13 18:45:11 -0400

    e

--- package/Events.js
+++ package/Events.js
@@ -2,29 +2,31 @@ import axios from "axios";
 import { Event } from './Event.js'
 class Events {
     getRecentEvents() {
-        return new Promise((res) => {
+        return new Promise((rez) => {
             axios.get('https://api.rec.net/api/playerevents/v1').then(res => {
                 const events = [];
                 res.data.forEach(event => {
                     events.push(new Event(event.PlayerEventId, event.CreatorPlayerId, event.ImageName, event.RoomId, event.SubRoomId, event.ClubId, event.Name, event.Description, event.StartTime, event.EndTime, event.AttendeeCount, event.State, event.Accessibility, event.IsMultiInstance, event.SupportsMultiInstanceRoomChat, event.DefaultBroadcastPremissions, event.CanRequestBroadcastPermissions));
                 })
-                res(events);
+                rez(events);
             });
         })
     }
     getEventByID(id) {
-        return new Promise((res) => {
+        return new Promise((rez) => {
             axios.get(`https://api.rec.net/api/playerevents/v1/${id}`).then(res => {
-
-                res(new Event(res.data.Playerres.dataId, res.data.CreatorPlayerId, res.data.ImageName, res.data.RoomId, res.data.SubRoomId, res.data.ClubId, res.data.Name, res.data.Description, res.data.StartTime, res.data.EndTime, res.data.AttendeeCount, res.data.State, res.data.Accessibility, res.data.IsMultiInstance, res.data.SupportsMultiInstanceRoomChat, res.data.DefaultBroadcastPremissions, res.data.CanRequestBroadcastPermissions));
+                rez(new Event(res.data.Playerres.dataId, res.data.CreatorPlayerId, res.data.ImageName, res.data.RoomId, res.data.SubRoomId, res.data.ClubId, res.data.Name, res.data.Description, res.data.StartTime, res.data.EndTime, res.data.AttendeeCount, res.data.State, res.data.Accessibility, res.data.IsMultiInstance, res.data.SupportsMultiInstanceRoomChat, res.data.DefaultBroadcastPremissions, res.data.CanRequestBroadcastPermissions));
             });
         })
     }
-    getEventsInRoom(RoomID) {
-        return new Promise((res) => {
-            axios.get(`https://api.rec.net/api/playerevents/v1/room/${id}`).then(res => {
-
-                res(new Event(res.data.Playerres.dataId, res.data.CreatorPlayerId, res.data.ImageName, res.data.RoomId, res.data.SubRoomId, res.data.ClubId, res.data.Name, res.data.Description, res.data.StartTime, res.data.EndTime, res.data.AttendeeCount, res.data.State, res.data.Accessibility, res.data.IsMultiInstance, res.data.SupportsMultiInstanceRoomChat, res.data.DefaultBroadcastPremissions, res.data.CanRequestBroadcastPermissions));
+    getEventsInRoom(RoomID, take) {
+        return new Promise(rez => {
+            axios.get(`https://api.rec.net/api/playerevents/v1/room/${RoomID}?take=${take}`).then(res => {
+                const events = [];
+                res.data.forEach(event => {
+                    events.push(new Event(event.PlayerEventId, event.CreatorPlayerId, event.ImageName, event.RoomId, event.SubRoomId, event.ClubId, event.Name, event.Description, event.StartTime, event.EndTime, event.AttendeeCount, event.State, event.Accessibility, event.IsMultiInstance, event.SupportsMultiInstanceRoomChat, event.DefaultBroadcastPremissions, event.CanRequestBroadcastPermissions));
+                })
+                rez(events);
             });
         })
     }
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 import { rooms, players, friends, events } from 'recnetjs'
-//rooms.getRoomByName('UknownArmy').then((res) => console.log(res))
+rooms.getRoomByName('UnknownArmy').then((res) => events.getEventsInRoom(res.RoomID).then(ev => {console.log(ev)}))
 //players.getPlayerByID('1').then(async (res) => console.log(await res.getImage()))
-events.getRecentEvents()
+//events.getRecentEvents()
